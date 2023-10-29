a5d2f7216fd541e8050e6ba38d23100592ad8a9c
diegodias <emerson.diego.duarte@gmail.com>
2023-10-29 18:51:09 -0300

    GitHub actions

--- providers.tf
+++ providers.tf
@@ -1,13 +1,34 @@
 terraform {
+
+  required_version = "~> 1.3"
   required_providers {
     aws = {
       source = "hashicorp/aws"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+  }
+
+  cloud {
+    workspaces {
+      name = "terraform-aws"
     }
   }
 }
 
 provider "aws" {
   region                   = "us-east-1"
-  access_key = "AKIAR73XG2I23OWYBYEZ"
-  secret_key = "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"
 }
